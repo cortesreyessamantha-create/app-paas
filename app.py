dbaab4964c8e6ec8019678cd31961ba5c4247926
cortesreyessamantha-create <cortesreyessamantha@gmail.com>
2026-03-13 01:04:33 -0600

    Update app.py
    Refactor Flask app to use environment variable for port.

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
+import os
 
-@app.route('/')
-def hello():
-    return "Hola desde PaaS en AWS"
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
